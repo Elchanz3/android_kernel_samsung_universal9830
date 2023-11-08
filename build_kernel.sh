1531d127687bb019b8e4bfb7b7d3477d2a605ce0
snapdragon teaks <inutilidades639@gmail.com>
2023-11-07 22:38:54 -0300

    improve build script by 1000%

--- build_kernel.sh
+++ build_kernel.sh
@@ -1,12 +1,18 @@
 #!/bin/bash
 
+# Function to display error messages and exit
+function error_exit {
+    echo "Error: $1" >&2
+    exit 1
+}
+
 clear
 
 # Variables
-DIR=`readlink -f .`;
-PARENT_DIR=`readlink -f ${DIR}/..`;
+DIR=$(readlink -f .)
+PARENT_DIR=$(readlink -f "${DIR}/..")
 
-DEFCONFIG_NAME=exynos9830-c1sxxx_defconfig
+DEFCONFIG_NAME=chanz_c1s_defconfig
 CHIPSET_NAME=universal9830
 VARIANT=c1s
 ARCH=arm64
@@ -14,46 +20,52 @@ VERSION=PuppyKernel_${VARIANT}_v1.2
 LOG_FILE=compilation.log
 CCACHE=ccache
 
-mkdir out
-
-DTB_DIR=$(pwd)/out/arch/arm64/boot/dts
-mkdir ${DTB_DIR}/exynos
-
 export PLATFORM_VERSION=13
 export ANDROID_MAJOR_VERSION=t
 export SEC_BUILD_CONF_VENDOR_BUILD_OS=13
 
-BUILD_CROSS_COMPILE=/home/chanz22/tc/aarch64-zyc-linux-gnu-14/bin/aarch64-zyc-linux-gnu-
-KERNEL_LLVM_BIN=/home/chanz22/tc/Clang-14.0.6-20230207/bin/clang
-CLANG_TRIPLE=/home/chanz22/tc/aarch64-zyc-linux-gnu-14/bin/aarch64-zyc-linux-gnu-
+# Check for the existence of output directories
+OUT_DIR="out"
+DTB_DIR="$OUT_DIR/arch/arm64/boot/dts/exynos"
+mkdir -p "$DTB_DIR" || error_exit "Failed to create directory $DTB_DIR"
 
+# Toolchains
+BUILD_CROSS_COMPILE="/home/chanz22/tc/aarch64-zyc-linux-gnu-14/bin/aarch64-zyc-linux-gnu-"
+KERNEL_LLVM_BIN="/home/chanz22/tc/Clang-14.0.6-20230207/bin/clang"
+CLANG_TRIPLE="$BUILD_CROSS_COMPILE"
+
+# Cleaning (if -clean flag is provided)
+if [[ "$1" == "-clean" ]]; then
+    echo "Cleaning before building..."
+    make O="$OUT_DIR" ARCH=arm64 clean || error_exit "Error while running 'make clean'"
+    make O="$OUT_DIR" ARCH=arm64 mrproper || error_exit "Error while running 'make mrproper'"
+fi
+
+# Kernel compilation
 DATE_START=$(date +"%s")
 
-make O=out ARCH=arm64 CC=$KERNEL_LLVM_BIN $DEFCONFIG_NAME
-make O=out ARCH=arm64 \
-	CROSS_COMPILE=$BUILD_CROSS_COMPILE CC=$KERNEL_LLVM_BIN \
-	CLANG_TRIPLE=$CLANG_TRIPLE -j$(nproc) 2>&1 |tee ../$LOG_FILE
+make O="$OUT_DIR" ARCH=arm64 CC="$KERNEL_LLVM_BIN" "$DEFCONFIG_NAME" || error_exit "Error configuring the kernel"
+make O="$OUT_DIR" ARCH=arm64 CROSS_COMPILE="$BUILD_CROSS_COMPILE" CC="$KERNEL_LLVM_BIN" CLANG_TRIPLE="$CLANG_TRIPLE" -j"$(nproc)" 2>&1 | tee "../$LOG_FILE" || error_exit "Error during kernel compilation"
 
-# remove a previous kernel image
-rm $IMAGE &> /dev/null
+# Verification and packaging
+IMAGE="$OUT_DIR/arch/arm64/boot/Image"
 
-$(pwd)/tools/mkdtimg cfg_create $(pwd)/out/dtb.img dt.configs/exynos9830.cfg -d ${DTB_DIR}/exynos
-
-IMAGE="out/arch/arm64/boot/Image"
 if [[ -f "$IMAGE" ]]; then
-        KERNELZIP="$VERSION.zip"
-	rm AnyKernel3/zImage > /dev/null 2>&1
-	rm AnyKernel3/dtb > /dev/null 2>&1
-	rm AnyKernel3/*.zip > /dev/null 2>&1
-	mv out/dtb.img AnyKernel3/dtb
-	mv $IMAGE AnyKernel3/zImage
-	cd AnyKernel3
-	zip -r9 $KERNELZIP .
+    KERNELZIP="$VERSION.zip"
+    rm "AnyKernel3/zImage" "AnyKernel3/dtb" "AnyKernel3/"*.zip &>/dev/null
 
-	DATE_END=$(date +"%s")
-	DIFF=$(($DATE_END - $DATE_START))
+    $(pwd)/tools/mkdtimg cfg_create $(pwd)/"$OUT_DIR/dtb.img" dt.configs/exynos9830.cfg -d "$DTB_DIR"
+    mv "$OUT_DIR/dtb.img" "AnyKernel3/dtb"
+    mv "$IMAGE" "AnyKernel3/zImage"
 
-	echo -e "\nTime elapsed: $(($DIFF / 60)) minute(s) and $(($DIFF % 60)) seconds.\n"
+    # Create the AnyKernel package
+    (cd "AnyKernel3" && zip -r9 "$KERNELZIP" .) || error_exit "Error creating the AnyKernel package"
 
+    DATE_END=$(date +"%s")
+    DIFF=$(($DATE_END - $DATE_START))
 
+    echo -e "\nCompilation completed successfully. Elapsed time: $(($DIFF / 60)) minute(s) and $(($DIFF % 60)) seconds.\n"
+else
+    error_exit "Kernel image file not found. Compilation may have failed."
 fi
+
